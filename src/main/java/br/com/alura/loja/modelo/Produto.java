c268790041da6da210c6a6cde87ee019138c01dd
Weverton Freitas Perdigão <weverton.perdigao@itbam.org.br>
2021-05-07 15:15:55 -0400

    update class

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,84 +1,86 @@
 package br.com.alura.loja.modelo;
 
-
-import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "produtos")
 public class Produto {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
-    @Column(name = "descricao")//quando for diferente
-    private String descricao;
-    private BigDecimal preco;
-    private LocalDate dataCadastro = LocalDate.now();
-    @ManyToOne
-    private Categoria categoria;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private String descricao;
+	private BigDecimal preco;
+	private LocalDate dataCadastro = LocalDate.now();
+	
+	@ManyToOne
+	private Categoria categoria;
+	
+	public Produto() {
+	}
+	
+	public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+		this.categoria = categoria;
+	}
 
-    public Produto(String nome, String descricao, BigDecimal preco, Categoria categoria) {
-        this.nome = nome;
-        this.descricao = descricao;
-        this.preco = preco;
-        this.categoria = categoria;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public Produto() {
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
+	public String getNome() {
+		return nome;
+	}
 
-    public LocalDate getDataCadastro() {
-        return dataCadastro;
-    }
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 
-    public void setDataCadastro(LocalDate dataCadastro) {
-        this.dataCadastro = dataCadastro;
-    }
+	public String getDescricao() {
+		return descricao;
+	}
 
-    public Categoria getCategoria() {
-        return categoria;
-    }
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
 
-    public void setCategoria(Categoria categoria) {
-        this.categoria = categoria;
-    }
+	public BigDecimal getPreco() {
+		return preco;
+	}
 
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public LocalDate getDataCadastro() {
+		return dataCadastro;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setDataCadastro(LocalDate dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
 
-    public String getNome() {
-        return nome;
-    }
+	public Categoria getCategoria() {
+		return categoria;
+	}
 
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public BigDecimal getPreco() {
-        return preco;
-    }
-
-    public void setPreco(BigDecimal preco) {
-        this.preco = preco;
-    }
-
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
 
 }
